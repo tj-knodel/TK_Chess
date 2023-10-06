4d0acba63b443d432e1d598e4a76efaa9906e579
Thaler Knodel <thaler.knodel.stu@kingsu.ca>
2023-10-06 16:44:19 -0600

    added AIplayer stub

---
+++ src/edu/kingsu/SoftwareEngineering/Chess/AIPlayer/AIPlayer.java
@@ -0,0 +1,12 @@
+package edu.kingsu.SoftwareEngineering.Chess.AIPlayer;
+
+public class AIPlayer {
+    /**
+     * gets a move from the AI Player
+     * 
+     * @author Thaler Knodel
+     */
+    public void getMove() {
+
+    }
+}
