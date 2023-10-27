a0f97f84c49461e6356882182821f9e0ff4505ba
Daniell Buchner <daniell.buchner.stu@kingsu.ca>
2023-10-27 14:41:44 -0600

    Added GameMode

---
+++ src/edu/kingsu/SoftwareEngineering/Chess/GameMode/GameMode.java
@@ -0,0 +1,9 @@
+package edu.kingsu.SoftwareEngineering.Chess.GameMode;
+
+public abstract class GameMode {
+    public abstract void switchTeam();
+
+    public abstract void startGame();
+
+    public abstract void endGame();
+}
