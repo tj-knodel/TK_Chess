39f22127efe3cb73f8fe22b5d374dcb0f0273f30
Thaler Knodel <thalerknodel@gmail.com>
2023-11-08 10:18:27 -0700

    adding unit teseting for the board

--- src/edu/kingsu/SoftwareEngineering/Chess/Players/AIThread.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/Players/AIThread.java
@@ -1,8 +1,29 @@
 package edu.kingsu.SoftwareEngineering.Chess.Players;
+import edu.kingsu.SoftwareEngineering.Chess.Board.Board;
 
 /**
  * A wrapper to store an AIPlayer
  */
-public class AIThread {
-    private Player AIPlayer;
+public class AIThread implements Runnable {
+    private Player aiPlayer;
+    private Board board;
+    private Move curMove;
+
+    public AIThread(AIPlayer ai, Board board) {
+        aiPlayer = ai;
+        this.board = board;
+        curMove = null;
+    }
+    /**
+     * This function will 
+     */
+    @Override
+    public void run() {
+        // simple loop to just keep checking if we need a move and update the listener.
+        curMove = aiPlayer.getMove(board);
+    }
+
+    public Move getMove() {
+        return curMove;
+    }
 }
