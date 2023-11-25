ac1394e4065db776eb2a865e01fe5a7a2880bb02
Daniell Buchner <daniell.buchner.stu@kingsu.ca>
2023-11-24 20:34:25 -0700

    Changed king castling check to not simulate

--- src/edu/kingsu/SoftwareEngineering/Chess/Board/Pieces/King.java
+++ src/edu/kingsu/SoftwareEngineering/Chess/Board/Pieces/King.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import edu.kingsu.SoftwareEngineering.Chess.Board.Board;
 import edu.kingsu.SoftwareEngineering.Chess.Board.BoardLocation;
 import edu.kingsu.SoftwareEngineering.Chess.Board.MoveValidity;
+import edu.kingsu.SoftwareEngineering.Chess.Board.Team;
 
 /**
  * @author Daniell Buchner
@@ -76,27 +77,52 @@ public class King extends Piece {
         if (hasMoved)
             return false;
         Piece[][] boardCopy = board.getBoard();
-        // Only ever one king per team.
         BoardLocation kingLocation = board.getBoardLocationsForTeamForPiece(board.getBoard(), team, Piece.KING).get(0);
-        Piece pieceOne = boardCopy[kingLocation.row][kingLocation.column + 1];
-        Piece pieceTwo = boardCopy[kingLocation.row][kingLocation.column + 2];
-        if (!(pieceOne instanceof EmptyPiece && pieceTwo instanceof EmptyPiece))
+        BoardLocation loc1 = new BoardLocation(kingLocation.column + 1, kingLocation.row);
+        BoardLocation loc2 = new BoardLocation(kingLocation.column + 2, kingLocation.row);
+        Piece piece1 = boardCopy[loc1.row][loc1.column];
+        Piece piece2 = boardCopy[loc2.row][loc2.column];
+
+        BoardLocation rookPos = new BoardLocation(kingLocation.column + 3, kingLocation.row);
+        Piece rookPiece = boardCopy[rookPos.row][rookPos.column];
+
+        if (!(piece1 instanceof EmptyPiece) || !(piece2 instanceof EmptyPiece) || !(rookPiece instanceof Rook))
             return false;
-        Rook rook = (Rook) (boardCopy[kingLocation.row][kingLocation.column + 3]);
-        if (rook.hasMoved())
+
+        if (((Rook) rookPiece).hasMoved())
             return false;
-        // Simulate the moves now.
-        board.simulateApplyMove(boardCopy, boardCopy[kingLocation.row][kingLocation.column], kingLocation,
-                new BoardLocation(kingLocation.column + 1, kingLocation.row));
-        King king = (King) (boardCopy[kingLocation.row][kingLocation.column + 1]);
-        if (king.inCheck)
-            return false;
-        kingLocation = new BoardLocation(kingLocation.column + 1, kingLocation.row);
-        board.simulateApplyMove(boardCopy, boardCopy[kingLocation.row][kingLocation.column], kingLocation,
-                new BoardLocation(kingLocation.column + 1, kingLocation.row));
-        king = (King) (boardCopy[kingLocation.row][kingLocation.column + 1]);
-        if (king.inCheck)
-            return false;
+
+        for (BoardLocation teamLoc : board.getBoardLocationsForTeam(boardCopy, Team.getOtherTeam(team))) {
+            if (boardCopy[teamLoc.row][teamLoc.column] instanceof King)
+                continue;
+            for (BoardLocation otherLoc : boardCopy[teamLoc.row][teamLoc.column].getPossibleMoves(board, boardCopy,
+                    teamLoc, false)) {
+                if (otherLoc.isEqual(loc1) || otherLoc.isEqual(loc2))
+                    return false;
+            }
+        }
+
+        // // Only ever one king per team.
+        // BoardLocation kingLocation = board.getBoardLocationsForTeamForPiece(board.getBoard(), team, Piece.KING).get(0);
+        // Piece pieceOne = boardCopy[kingLocation.row][kingLocation.column + 1];
+        // Piece pieceTwo = boardCopy[kingLocation.row][kingLocation.column + 2];
+        // if (!(pieceOne instanceof EmptyPiece && pieceTwo instanceof EmptyPiece))
+        //     return false;
+        // Rook rook = (Rook) (boardCopy[kingLocation.row][kingLocation.column + 3]);
+        // if (rook.hasMoved())
+        //     return false;
+        // // Simulate the moves now.
+        // board.simulateApplyMove(boardCopy, boardCopy[kingLocation.row][kingLocation.column], kingLocation,
+        //         new BoardLocation(kingLocation.column + 1, kingLocation.row));
+        // King king = (King) (boardCopy[kingLocation.row][kingLocation.column + 1]);
+        // if (king.inCheck)
+        //     return false;
+        // kingLocation = new BoardLocation(kingLocation.column + 1, kingLocation.row);
+        // board.simulateApplyMove(boardCopy, boardCopy[kingLocation.row][kingLocation.column], kingLocation,
+        //         new BoardLocation(kingLocation.column + 1, kingLocation.row));
+        // king = (King) (boardCopy[kingLocation.row][kingLocation.column + 1]);
+        // if (king.inCheck)
+        //     return false;
         return true;
     }
 
@@ -109,34 +135,61 @@ public class King extends Piece {
         if (hasMoved)
             return false;
         Piece[][] boardCopy = board.getBoard();
-        // Only ever one king per team.
         BoardLocation kingLocation = board.getBoardLocationsForTeamForPiece(board.getBoard(), team, Piece.KING).get(0);
-        Piece pieceOne = boardCopy[kingLocation.row][kingLocation.column - 1];
-        Piece pieceTwo = boardCopy[kingLocation.row][kingLocation.column - 2];
-        Piece pieceThree = boardCopy[kingLocation.row][kingLocation.column - 3];
-        if (!(pieceOne instanceof EmptyPiece && pieceTwo instanceof EmptyPiece && pieceThree instanceof EmptyPiece))
+        BoardLocation loc1 = new BoardLocation(kingLocation.column - 1, kingLocation.row);
+        BoardLocation loc2 = new BoardLocation(kingLocation.column - 2, kingLocation.row);
+        BoardLocation loc3 = new BoardLocation(kingLocation.column - 3, kingLocation.row);
+        Piece piece1 = boardCopy[loc1.row][loc1.column];
+        Piece piece2 = boardCopy[loc2.row][loc2.column];
+        Piece piece3 = boardCopy[loc3.row][loc3.column];
+
+        BoardLocation rookPos = new BoardLocation(kingLocation.column - 4, kingLocation.row);
+        Piece rookPiece = boardCopy[rookPos.row][rookPos.column];
+
+        if (!(piece1 instanceof EmptyPiece) || !(piece2 instanceof EmptyPiece) || !(piece3 instanceof EmptyPiece)
+                || !(rookPiece instanceof Rook))
             return false;
-        Rook rook = (Rook) (boardCopy[kingLocation.row][kingLocation.column - 4]);
-        if (rook.hasMoved())
+
+        if (((Rook) rookPiece).hasMoved())
             return false;
-        // Simulate the moves now.
-        board.simulateApplyMove(boardCopy, boardCopy[kingLocation.row][kingLocation.column], kingLocation,
-                new BoardLocation(kingLocation.column - 1, kingLocation.row));
-        King king = (King) (boardCopy[kingLocation.row][kingLocation.column - 1]);
-        if (king.inCheck)
-            return false;
-        kingLocation = new BoardLocation(kingLocation.column - 1, kingLocation.row);
-        board.simulateApplyMove(boardCopy, boardCopy[kingLocation.row][kingLocation.column], kingLocation,
-                new BoardLocation(kingLocation.column - 1, kingLocation.row));
-        king = (King) (boardCopy[kingLocation.row][kingLocation.column - 1]);
-        if (king.inCheck)
-            return false;
-        kingLocation = new BoardLocation(kingLocation.column - 1, kingLocation.row);
-        board.simulateApplyMove(boardCopy, boardCopy[kingLocation.row][kingLocation.column], kingLocation,
-                new BoardLocation(kingLocation.column - 1, kingLocation.row));
-        king = (King) (boardCopy[kingLocation.row][kingLocation.column - 1]);
-        if (king.inCheck)
-            return false;
+
+        for (BoardLocation teamLoc : board.getBoardLocationsForTeam(boardCopy, Team.getOtherTeam(team))) {
+            if (boardCopy[teamLoc.row][teamLoc.column] instanceof King)
+                continue;
+            for (BoardLocation otherLoc : boardCopy[teamLoc.row][teamLoc.column].getPossibleMoves(board, boardCopy,
+                    teamLoc, false)) {
+                if (otherLoc.isEqual(loc1) || otherLoc.isEqual(loc2) || otherLoc.isEqual(loc3))
+                    return false;
+            }
+        }
+        // Only ever one king per team.
+        // BoardLocation kingLocation = board.getBoardLocationsForTeamForPiece(board.getBoard(), team, Piece.KING).get(0);
+        // Piece pieceOne = boardCopy[kingLocation.row][kingLocation.column - 1];
+        // Piece pieceTwo = boardCopy[kingLocation.row][kingLocation.column - 2];
+        // Piece pieceThree = boardCopy[kingLocation.row][kingLocation.column - 3];
+        // if (!(pieceOne instanceof EmptyPiece && pieceTwo instanceof EmptyPiece && pieceThree instanceof EmptyPiece))
+        //     return false;
+        // Rook rook = (Rook) (boardCopy[kingLocation.row][kingLocation.column - 4]);
+        // if (rook.hasMoved())
+        //     return false;
+        // // Simulate the moves now.
+        // board.simulateApplyMove(boardCopy, boardCopy[kingLocation.row][kingLocation.column], kingLocation,
+        //         new BoardLocation(kingLocation.column - 1, kingLocation.row));
+        // King king = (King) (boardCopy[kingLocation.row][kingLocation.column - 1]);
+        // if (king.inCheck)
+        //     return false;
+        // kingLocation = new BoardLocation(kingLocation.column - 1, kingLocation.row);
+        // board.simulateApplyMove(boardCopy, boardCopy[kingLocation.row][kingLocation.column], kingLocation,
+        //         new BoardLocation(kingLocation.column - 1, kingLocation.row));
+        // king = (King) (boardCopy[kingLocation.row][kingLocation.column - 1]);
+        // if (king.inCheck)
+        //     return false;
+        // kingLocation = new BoardLocation(kingLocation.column - 1, kingLocation.row);
+        // board.simulateApplyMove(boardCopy, boardCopy[kingLocation.row][kingLocation.column], kingLocation,
+        //         new BoardLocation(kingLocation.column - 1, kingLocation.row));
+        // king = (King) (boardCopy[kingLocation.row][kingLocation.column - 1]);
+        // if (king.inCheck)
+        //     return false;
         return true;
     }
 
@@ -148,13 +201,13 @@ public class King extends Piece {
             boolean extraCheck) {
         ArrayList<BoardLocation> moves = new ArrayList<>();
         BoardLocation endMove = new BoardLocation(startMove.column, startMove.row);
+        if (canCastleLongSide(boardClass)) {
+            moves.add(new BoardLocation(startMove.column - 4, startMove.row));
+        }
+        if (canCastleShortSide(boardClass)) {
+            moves.add(new BoardLocation(startMove.column + 3, startMove.row));
+        }
         if (extraCheck) {
-            if (canCastleLongSide(boardClass)) {
-                moves.add(new BoardLocation(startMove.column - 4, startMove.row));
-            }
-            if (canCastleShortSide(boardClass)) {
-                moves.add(new BoardLocation(startMove.column + 3, startMove.row));
-            }
         }
 
         // Top Left
